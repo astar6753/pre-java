d9de0c7b4eede7fd00fa5462d260ef68eeece2fc
astar6753 <whddbs6753@naver.com>
2022-11-07 13:16:26 +0900

    PeerSession -ValidatorTest  exception처리 test

--- PeerSession/ValidatorTest.java
+++ PeerSession/ValidatorTest.java
@@ -9,38 +9,36 @@ public class ValidatorTest {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-        String str1 = br.readLine();
+        String str = br.readLine();
 
-        if(str1.isEmpty() || hasWhiteSpace(str1)) {
+        if(str.isEmpty() || hasWhiteSpace(str)) {
             throw new IllegalArgumentException("NOT_INPUT_BLANK", new Throwable("입력값이 비어있거나, 공백이 있습니다."));
         }
 
         Pattern regex = Pattern.compile("^([1-9]{3})$");
-        if(!str1.matches(regex.toString())) {
+        if(!str.matches(regex.toString())) {
             throw new IllegalArgumentException("NOT_INPUT_STRING", new Throwable("입력값은 3자리 정수(1-9)만 가능합니다."));
         }
 
-        if(!isValidLength(str1, 3)){
+        if(!isValidLength(str, 3)){
             throw new IllegalArgumentException("INPUT_LIMIT_THREE", new Throwable("입력값이 3자리를 넘습니다. (입력값은 3자리 까지만 가능합니다.)"));
         }
         try {
-            Integer.parseInt(str1);
+            Integer.parseInt(str);
         } catch (NumberFormatException ex1) {
             throw new IllegalArgumentException("NOT_INPUT_STRING", new Throwable("입력값에  문자가 있습니다. (입력값은 정수만 가능합니다.)"));
         }
 
 
-        System.out.println(str1);
+        System.out.println(str);
     }
 
     static Boolean hasWhiteSpace(String str) {
-        if(str.length() == str.replace(" ", "").length()) { return false; }
-        return true;
+        return str.length() != str.replace(" ", "").length();
     }
 
     static Boolean isValidLength(String str, int len) {
-        if(str.length() != len) { return false; }
-        return true;
+        return str.length() == len;
     }
 }
 
